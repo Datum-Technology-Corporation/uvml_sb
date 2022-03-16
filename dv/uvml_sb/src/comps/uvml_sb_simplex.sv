a78f081113a1b8a050966f16ff9344c57e6acdfd
datum-dpoulin <dpoulin@datumtc.ca>
2022-03-16 18:24:17 -0400

    fix to logging if no initiator is set

--- dv/uvml_sb/src/comps/uvml_sb_simplex.sv
+++ dv/uvml_sb/src/comps/uvml_sb_simplex.sv
@@ -490,14 +490,24 @@ endfunction : calc_exp_stats
 
 function void uvml_sb_simplex_c::log_new_act(ref T_ACT_TRN trn);
    
-   `uvm_info("SB_SIMPLEX", $sformatf("New actual transaction from %s:\n%s", trn.get_initiator().get_full_name(), trn.sprint()), UVM_HIGH)
+   if (trn.get_initiator() != null) begin
+      `uvm_info("SB_SIMPLEX", $sformatf("New actual transaction from %s:\n%s", trn.get_initiator().get_full_name(), trn.sprint()), UVM_HIGH)
+   end
+   else begin
+      `uvm_info("SB_SIMPLEX", $sformatf("New actual transaction:\n%s", trn.sprint()), UVM_HIGH)
+   end
    
 endfunction : log_new_act
 
 
 function void uvml_sb_simplex_c::log_new_exp(ref T_EXP_TRN trn);
    
-   `uvm_info("SB_SIMPLEX", $sformatf("New expected transaction from %s:\n%s", trn.get_initiator().get_full_name(), trn.sprint()), UVM_HIGH)
+   if (trn.get_initiator() != null) begin
+      `uvm_info("SB_SIMPLEX", $sformatf("New expected transaction from %s:\n%s", trn.get_initiator().get_full_name(), trn.sprint()), UVM_HIGH)
+   end
+   else begin
+      `uvm_info("SB_SIMPLEX", $sformatf("New expected transaction:\n%s", trn.sprint()), UVM_HIGH)
+   end
    
 endfunction : log_new_exp
 
